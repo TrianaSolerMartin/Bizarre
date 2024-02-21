a8e9d46b5e87738999208c2518b69a525e366f63
Juan Munguia <carlosmunguia23145@gmail.com>
2024-02-21 15:37:41 +0100

    ok

---
+++ backend/src/main/java/com/example/backend/config/StorageProperties.java
@@ -0,0 +1,18 @@
+package com.example.backend.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StorageProperties {
+    
+    // String location = "src/main/resources/static/images";
+
+    // public String getLocation() {
+    //     return location;
+    // }
+
+    // public void setLocation(String location) {
+    //     this.location = location;
+    // }
+
+}
