0a0e125b94b15d5d1526418b39589369296ed960
TrianaSolerMartin <triana287@gmail.com>
2024-02-21 19:01:22 +0100

    adding

---
+++ Frond-end/src/router/router.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+import LayoutPublic from "../components/layout";
+import Home from "../components/home/Home";
+import NewItem from "../components/newitem/NewItem";
+import Edit from "../components/edit/Edit";
+import Footer from "../components/footer/footer";
+import Gallery from "../components/gallery/Gallery";
+import Card from "../components/card/Card";
+import ImageUpload from "../components/ImageUpload/imgupload";
+
+
+export const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <LayoutPublic />,
+      children: [
+    {
+      path: "/",
+      element: <Home/>,
+    },
+
+    {
+      path: "/footer",
+      element: <Footer/>
+    },
+    {
+      path: "/NewItem",
+      element: <NewItem/>,
+    },
+    {
+      path: "/Edit/:id",
+      element: <Edit/>
+    },
+    {
+      path: "/Gallery",
+      element: <Gallery/>
+    },
+    {
+      path: "/card/:id",
+      element: <Card/>
+    },
+    {
+      path: "/imgupload/",
+      element: <ImageUpload/>
+    }
+  ],
+  }
+  ]);
+
+export default router;
