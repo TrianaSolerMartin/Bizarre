0e58602f282c9214c83ab5136895a0e6f3b2f66b
Hack3r-han <hannahphil27@gmail.com>
2024-02-22 09:31:13 +0100

    Add comments to highlight where our database will go.

--- Front-end/src/page/Products.jsx
+++ Front-end/src/page/Products.jsx
@@ -25,7 +25,7 @@ function ProductList (props) {
 
     function fetchProducts() {
 
-            fetch("https://api-e-collectic.vercel.app/products")
+            fetch("https://api-e-collectic.vercel.app/products") //CAMBIA PARA NUESTRO BBDD
             .then((response) => {
                 if (!response.ok) {
                     throw new Error("Unexpected Server Response")
@@ -45,7 +45,7 @@ function ProductList (props) {
     useEffect(() => fetchProducts(), []);
 
     function deleteProduct(id) {
-        fetch("https://api-e-collectic.vercel.app/products/" + id, {
+        fetch("https://api-e-collectic.vercel.app/products/" + id, { //CAMBIA PARA NUESTRO BBDD
             method: "DELETE",
         })
         .then((response) => {
@@ -128,7 +128,7 @@ function ProductForm(props) {
     
         if (props.product.id) {
             // update the product
-            fetch("https://api-e-collectic.vercel.app/products/" + props.product.id, {
+            fetch("https://api-e-collectic.vercel.app/products/" + props.product.id, { //CAMBIA PARA NUESTRO BBDD
                 method: "PATCH",
                 headers: {
                     "Content-Type": "application/json",
@@ -152,7 +152,7 @@ function ProductForm(props) {
         
             // create a new product
             product.createdAt = new Date().toISOString().slice(0, 10);
-            fetch("https://api-e-collectic.vercel.app/products", {
+            fetch("https://api-e-collectic.vercel.app/products", { //CAMBIA PARA NUESTRO BBDD
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
